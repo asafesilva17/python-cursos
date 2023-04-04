fa495b0af0b6902e5186abe4c5f965fa5b06e25a
asafesilva17 <asafesilvadedeus165@gmail.com>
2023-04-03 22:24:53 -0300

    imput,calc,multiply

---
+++ basic/input-calc.py
@@ -0,0 +1,25 @@
+def sum(val1,val2):
+    result = float(val1) + float(val2)
+    return str(result)
+
+def multiply(val1,val2):
+    result = float(val1) * float(val2)
+    return str(result)
+
+
+
+
+
+
+
+value1= input("digite o valor 1: ")
+value2= input("digite o valor 2: ")
+
+# result=sum(value1,value2)
+result=multiply(value1,value2)
+# result=subtract(value1,value2)
+# result=divide(value1,value2)
+print(result)
+
+
+
---
+++ basic/input.py
@@ -0,0 +1,3 @@
+text = input("gente boa,digite aqui:")
+print(text)
+
